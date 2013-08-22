e28cbb315da78c5f6329b8896ed98988d09701db
LeandroGuillen <leandroguillen@gmail.com>
2013-08-22 12:33:46 +0200

    Un juego puede ser privado/publico y encontrarse en diferentes estados

--- admin/models/game.js
+++ admin/models/game.js
@@ -1,7 +1,14 @@
 var mongoose = require('mongoose');
 
+var states = 'created running stopped deleted'.split(' ')
+
 exports.Game = mongoose.model('Game', new mongoose.Schema({
 	name: String,
 	master: String,
-	players: [String]
+	players: [String],
+	settings: {
+		state: { type: String, enum: states, default: "created" },
+		public: { type: Boolean, default: "true" }
+	}
+	
 }));

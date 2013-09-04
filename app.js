613198d62d5014ea101686df1da18d0d2d8bd7fd
LeandroGuillen <leandroguillen@gmail.com>
2013-09-04 15:01:51 +0200

    Ahora se enrutan las peticiones para el cliente web

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-
-var express = require('express')
-	, users = require('./admin/users')
-	, http = require('http')
-	, charsheet = require('./game/charsheet')
-	, game = require('./game/game')
-	, path = require('path')
-	, app = express()
-	, mongoose = require('mongoose');
+var express = require('express');
+var users = require('./admin/users');
+var routes = require('./web/routes');
+var http = require('http');
+var charsheet = require('./game/charsheet');
+var game = require('./game/game');
+var path = require('path');
+var app = express();
+var mongoose = require('mongoose');
 	
 mongoose.connect('mongodb://localhost/afserverdb');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/web/views');
+app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
 app.use(express.cookieSession({ key: 'afcookie', secret: "el conan tambien", cookie: { maxAge: 60 * 60 * 1000 }}));
+app.use(express.static(path.join(__dirname, 'web/public')));
 app.use(app.router);
 
 // development only
@@ -26,23 +29,25 @@ if ('development' == app.get('env')) {
 }
 
 // User operations
-app.post('/login', users.login);
-app.get('/logout', users.logout); 
-app.post('/user', users.register);
-app.get('/user/list', /*users.checkAuth,*/ users.list);
+app.post('/api/login', users.login);
+app.get('/api/logout', users.logout); 
+app.post('/api/user', users.register);
+app.get('/api/user/list', /*users.checkAuth,*/ users.list);
 
 // Charsheet operations
-app.get('/charsheet/list'/*, users.checkAuth*/, charsheet.list);
-app.get('/charsheet'/*, users.checkAuth*/, charsheet.current);
-app.post('/charsheet'/*, users.checkAuth*/, charsheet.new);
+app.get('/api/charsheet/list'/*, users.checkAuth*/, charsheet.list);
+app.get('/api/charsheet'/*, users.checkAuth*/, charsheet.current);
+app.post('/api/charsheet'/*, users.checkAuth*/, charsheet.new);
 
 // Game
-app.post('/game/start'/*, users.checkAuth*/, game.start);
-app.get('/game/join'/*, users.checkAuth*/, game.join);
-app.get('/game/leave'/*, users.checkAuth*/, game.leave);
-app.get('/game/end'/*, users.checkAuth*/, game.end);
-app.get('/game/list'/*, users.checkAuth*/, game.list);
+app.post('/api/game/start'/*, users.checkAuth*/, game.start);
+app.get('/api/game/join'/*, users.checkAuth*/, game.join);
+app.get('/api/game/leave'/*, users.checkAuth*/, game.leave);
+app.get('/api/game/end'/*, users.checkAuth*/, game.end);
+app.get('/api/game/list'/*, users.checkAuth*/, game.list);
 
+app.get('/', routes.index);
+app.get('/login', routes.login);
 
 // Run the party
 http.createServer(app).listen(app.get('port'), function(){

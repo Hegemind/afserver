89bc15cc6d277be5665876ee6dfd0fc76439c11f
Leandro Guillen <leandroguillen@gmail.com>
2013-12-03 00:31:21 +0100

    Fichero de pruebas de DB

--- scripts/generatedb.js
+++ scripts/generatedb.js
@@ -6,16 +6,23 @@ var db = new sqlite3.Database('../test.db', function(err){
 
 
 db.serialize(function() {
-	
-	var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-	for (var i = 0; i < 10; i++) {
-		stmt.run("Ipsum " + i);
-	}
-	stmt.finalize();
-
-	db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-		console.log(row.id + ": " + row.info);
+	db.each("SELECT * FROM user", function(err, row) {
+		if(err)
+			console.error(err);
+		else
+			console.log(row);
 	});
+	
+	
+// 	var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+// 	for (var i = 0; i < 10; i++) {
+// 		stmt.run("Ipsum " + i);
+// 	}
+// 	stmt.finalize();
+// 
+// 	db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+// 		console.log(row.id + ": " + row.info);
+// 	});
 });
 
 db.close();

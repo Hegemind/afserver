e66da93dc035631d2929f7cf6463c2d3f0917193
Leandro Guillen <leandroguillen@gmail.com>
2013-12-02 22:55:16 +0100

    Cambios menores

--- scripts/generatedb.js
+++ scripts/generatedb.js
@@ -7,9 +7,6 @@ var db = new sqlite3.Database('../test.db', function(err){
 
 db.serialize(function() {
 	
-	destruirTablas();
-	construirTablas();
-	
 	var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
 	for (var i = 0; i < 10; i++) {
 		stmt.run("Ipsum " + i);
@@ -22,43 +19,3 @@ db.serialize(function() {
 });
 
 db.close();
-
-
-
-
-function destruirTablas() {
-	// Todos los DROP
-	db.run("DROP SCHEMA IF EXISTS system;", function (err){
-		if(err)
-			console.error("No pude borrar el esquema system: "+err);
-		else
-			console.log("Borrada el esquema system");
-	});
-	
-	db.run("DROP TABLE IF EXISTS `system`.`game_system`", function (err){
-		if(err)
-			console.error("No pude borrar la tabla: "+err);
-		else
-			console.log("Borrada la tabla");
-	});
-
-	
-}
-
-function construirTablas() {
-	db.run("CREATE SCHEMA IF NOT EXISTS `system` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;", function (err){
-		if(err)
-			console.error("No pude crear el esquema system");
-		else
-			console.log("Creado el c system");
-	});
-	db.run("CREATE TABLE IF NOT EXISTS `system`.`game_system` (\
-	`id` INT NOT NULL ,\
-	`name` VARCHAR(32) NOT NULL ,\
-	PRIMARY KEY (`id`) );", function(err){
-		if(err)
-			console.error("No pude crear la tabla: "+err);
-		else 
-			console.log("Creada la tabla");
-	});
-}
